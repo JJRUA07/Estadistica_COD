e69082004476f684f6b0ec705e93d8b8269d0ecf
salas <salas@salas.local>
2025-02-10 09:53:33 -0500

    Se completo la interfaz grafica de usuario

--- src/frm_estadistica.java
+++ src/frm_estadistica.java
@@ -1,47 +1,100 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class frm_estadistica extends JFrame {
 
+    JTextField txtDato;
+    JList lstMuestra;
+    JTextField txtEstadistica;
+
     public frm_estadistica() {
 
-        //Formulario
-        setSize(600,300);
+        // Formulario
+        setSize(400, 300);
         setTitle("Estadistica");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        //Etiqueta
-        JLabel IdlDato=new JLabel("Dato");
+        // Etiqueta
+        JLabel IdlDato = new JLabel("Dato");
         IdlDato.setBounds(50, 10, 100, 25);
         getContentPane().add(IdlDato);
 
-        JLabel IdlMuestra=new JLabel("Muestra");
-        IdlMuestra.setBounds(300, 10, 100, 25);
+        JLabel IdlMuestra = new JLabel("Muestra");
+        IdlMuestra.setBounds(240, 10, 100, 25);
         getContentPane().add(IdlMuestra);
 
-        //Caja de texto
-        JTextField txtDato=new JTextField("");
+        // Caja de texto
+        txtDato = new JTextField("");
         txtDato.setBounds(110, 10, 100, 25);
         getContentPane().add(txtDato);
 
-        JTextField txtDesviacion=new JTextField("");
-        txtDesviacion.setBounds(250, 200, 100, 25);
-        getContentPane().add(txtDesviacion);
-        
-        //Botones
-        JButton btnAgregar=new JButton("Agregar");
+        JTextField txtEstadistica = new JTextField("");
+        txtEstadistica.setBounds(240, 190, 100, 25);
+        getContentPane().add(txtEstadistica);
+
+        // Botones
+        JButton btnAgregar = new JButton("Agregar");
         btnAgregar.setBounds(110, 50, 100, 25);
         getContentPane().add(btnAgregar);
 
-        JButton btnQuitar=new JButton("Quitar");
+        JButton btnQuitar = new JButton("Quitar");
         btnQuitar.setBounds(120, 80, 80, 25);
         getContentPane().add(btnQuitar);
 
-        JButton btnDesviacion=new JButton("Desviacion");
-        btnDesviacion.setBounds(140, 200, 100, 25);
-        getContentPane().add(btnDesviacion);
+        JButton btnEstadistica = new JButton("Calcular");
+        btnEstadistica.setBounds(40, 190, 100, 25);
+        getContentPane().add(btnEstadistica);
 
-        
+        // Lista
+        lstMuestra = new JList();
+        JScrollPane spMuestra=new JScrollPane(lstMuestra);
+        spMuestra.setBounds(240, 40, 110, 140);
+        getContentPane().add(spMuestra);
 
+        JComboBox cmbEstadistica=new JComboBox();
+        String[] opciones=new String[]{"Sumatoria", "Promedio", "Desviación", "Maximo", "Minimo", "Moda"};
+        DefaultComboBoxModel mdlEstadistica=new DefaultComboBoxModel(opciones);
+        cmbEstadistica.setModel(mdlEstadistica);
+        cmbEstadistica.setBounds(140,190,100,25);
+        getContentPane().add(cmbEstadistica);
+
+        // eventos de la GUI
+        btnAgregar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                agregarDato();
+            }
+        });
+
+        btnQuitar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                quitarDato();
+            }
+        });
+    }
+
+    // declarar el arreglo que almacenara los datos de la muestra
+    private double[] muestra = new double[1000];
+    private int totalDatos = -1;
+
+    private void agregarDato() {
+        double dato = Double.parseDouble(txtDato.getText());
+        totalDatos++;
+        muestra[totalDatos] = dato;
+        mostrarMuestra();
+    }
+
+    private void mostrarMuestra() {
+        String[] strMuestra = new String[totalDatos + 1];
+        for (int i = 0; i <= totalDatos; i++) {
+            strMuestra[i] = String.valueOf(muestra[i]);
+        }
+        lstMuestra.setListData(strMuestra);
+    }
+
+    private void quitarDato() {
+        JOptionPane.showMessageDialog(null, "Hizo clic en QUITAR");
     }
 }
